f56f024fd775ee2cf7878b023aea637e78b061f6
alexjhawk <alha@hms.se>
2021-08-02 10:18:18 -0400

    Add datapoint package

---
+++ src/main/java/com/hms_networks/americas/sc/extensions/datapoint/DataPointInteger.java
@@ -0,0 +1,106 @@
+package com.hms_networks.americas.sc.extensions.datapoint;
+
+/**
+ * Integer data point class
+ *
+ * <p>Class object for a DataPoint with a integer value
+ *
+ * @since 1.0.0
+ * @author HMS Networks, MU Americas Solution Center
+ */
+public class DataPointInteger extends DataPoint {
+
+  /** Data point value */
+  private final int value;
+
+  /**
+   * Constructor for a <code>integer</code> data point with specified quality.
+   *
+   * @param tagName data point tag name
+   * @param tagId data point tag ID
+   * @param value data point value
+   * @param time data point timestamp
+   * @param quality data point quality
+   */
+  public DataPointInteger(String tagName, int tagId, int value, String time, DataQuality quality) {
+    this.tagName = tagName;
+    this.tagId = tagId;
+    this.value = value;
+    this.timestamp = time;
+    this.quality = quality;
+  }
+
+  /**
+   * Constructor for a <code>integer</code> data point without specified quality.
+   *
+   * @param tagName data point tag name
+   * @param tagId data point tag ID
+   * @param value data point value
+   * @param time data point timestamp
+   */
+  public DataPointInteger(String tagName, int tagId, int value, String time) {
+    this.tagName = tagName;
+    this.tagId = tagId;
+    this.value = value;
+    this.timestamp = time;
+    this.quality = DataQuality.GOOD;
+  }
+
+  /**
+   * Get and return the data point value.
+   *
+   * @return data point value
+   */
+  public int getValue() {
+    return value;
+  }
+
+  /**
+   * Compares the data point to another data point.
+   *
+   * @param p data point to compare
+   * @return true if the timestamp, type, and value are the same
+   */
+  public boolean equals(DataPoint p) {
+    boolean returnVal = false;
+    if (p instanceof DataPointInteger) {
+      returnVal =
+          p.getTimeStamp().equals(timestamp)
+              && ((DataPointInteger) p).getValue() == value
+              && p.getTagName().equals(tagName);
+    }
+    return returnVal;
+  }
+
+  /**
+   * Compares the data point's value to another data point's value.
+   *
+   * @param p data point to compare
+   * @return true if values are equal
+   */
+  public boolean valueEquals(DataPoint p) {
+    boolean returnVal = false;
+    if (p instanceof DataPointInteger) {
+      returnVal = ((DataPointInteger) p).getValue() == value;
+    }
+    return returnVal;
+  }
+
+  /**
+   * Get the data point type.
+   *
+   * @return data point type
+   */
+  public DataType getType() {
+    return DataType.INTEGER;
+  }
+
+  /**
+   * Get the string representation of the data point value.
+   *
+   * @return data point value as a string
+   */
+  public String getValueString() {
+    return ("" + value);
+  }
+}
---
+++ src/main/java/com/hms_networks/americas/sc/extensions/datapoint/DataPointIntegerMappedString.java
@@ -0,0 +1,64 @@
+package com.hms_networks.americas.sc.extensions.datapoint;
+
+/**
+ * Class object for a {@link DataPoint} with an integer mapped (enumerated) {@link String}. Used to
+ * provide translated {@link String} values from an integer tag using an array of mappings.
+ *
+ * <p>Example (Integer Tag Value, Mapped String Value): <br>
+ * 0 = "Off" <br>
+ * 1 = "On, Loading" <br>
+ * 2 = "On, Ready" <br>
+ * 3 = "On, Processing" <br>
+ * 4 = "On, Completed" <br>
+ * 5 = "On, Failed" <br>
+ *
+ * @author HMS Networks, MU Americas Solution Center
+ * @since 1.0.0
+ */
+public class DataPointIntegerMappedString extends DataPointString {
+
+  /**
+   * Constructor for an integer mapped (enumerated) {@link String} data point with specified
+   * quality.
+   *
+   * @param tagName data point tag name
+   * @param tagId data point tag ID
+   * @param value data point value
+   * @param time data point timestamp
+   * @param quality data point quality
+   * @param enumMapping integer enumerated {@link String} mapping(s)
+   */
+  public DataPointIntegerMappedString(
+      String tagName,
+      int tagId,
+      int value,
+      String time,
+      DataQuality quality,
+      String[] enumMapping) {
+    super(tagName, tagId, enumMapping[value], time, quality);
+  }
+
+  /**
+   * Constructor for an integer mapped (enumerated) {@link String} data point without specified
+   * quality.
+   *
+   * @param tagName data point tag name
+   * @param tagId data point tag ID
+   * @param value data point value
+   * @param time data point timestamp
+   * @param enumMapping integer enumerated {@link String} mapping(s)
+   */
+  public DataPointIntegerMappedString(
+      String tagName, int tagId, int value, String time, String[] enumMapping) {
+    super(tagName, tagId, enumMapping[value], time);
+  }
+
+  /**
+   * Get the data point type.
+   *
+   * @return data point type
+   */
+  public DataType getType() {
+    return DataType.INTEGER_MAPPED_STRING;
+  }
+}
