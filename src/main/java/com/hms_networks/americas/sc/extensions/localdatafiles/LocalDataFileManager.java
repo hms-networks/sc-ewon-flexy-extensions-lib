c198400ae76c039d907519fcfc4bb50b815bcfc7
Alex Hawk <alha@hms.se>
2021-09-15 12:56:11 -0400

    Update data file date format to ISO 8601

--- src/main/java/com/hms_networks/americas/sc/extensions/localdatafiles/LocalDataFileManager.java
+++ src/main/java/com/hms_networks/americas/sc/extensions/localdatafiles/LocalDataFileManager.java
@@ -6,6 +6,7 @@ import com.hms_networks.americas.sc.extensions.datapoint.DataType;
 import com.hms_networks.americas.sc.extensions.fileutils.FileAccessManager;
 import com.hms_networks.americas.sc.extensions.fileutils.FileManager;
 import com.hms_networks.americas.sc.extensions.logging.Logger;
+import com.hms_networks.americas.sc.extensions.system.time.SCTimeFormat;
 import com.hms_networks.americas.sc.extensions.taginfo.TagInfoManager;
 import com.hms_networks.americas.sc.extensions.taginfo.TagType;
 import com.hms_networks.americas.sc.extensions.time.LocalTimeOffsetCalculator;
@@ -262,8 +263,8 @@ public class LocalDataFileManager extends Thread {
         localDataPointTime + LocalTimeOffsetCalculator.getLocalTimeOffsetMilliseconds();
 
     // Get local time from datapoint UTC time
-    String utcTimestamp = new Date(utcDataPointTime).toString();
-    String localTimestamp = new Date(localDataPointTime).toString();
+    String utcTimestamp = SCTimeFormat.ISO_8601.format(new Date(utcDataPointTime));
+    String localTimestamp = SCTimeFormat.ISO_8601.format(new Date(localDataPointTime));
 
     // Append UTC time and delimiter
     localDataFileWriteBuffer.append(utcTimestamp);
