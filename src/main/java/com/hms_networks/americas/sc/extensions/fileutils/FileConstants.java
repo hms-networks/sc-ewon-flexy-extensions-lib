b809247f8b89bb7a615d513952a2e0c438b01318
Alex J Hawk (HMS) <alha@hms.se>
2022-01-11 15:20:21 -0500

    Move file URL prefix constant
    Relocate the file URL prefix constant to a new
    file constants class to be used by other classes.

---
+++ src/main/java/com/hms_networks/americas/sc/extensions/fileutils/FileConstants.java
@@ -0,0 +1,14 @@
+package com.hms_networks.americas.sc.extensions.fileutils;
+
+/**
+ * Class for storing constants used in the {@link com.hms_networks.americas.sc.extensions.fileutils}
+ * package or associated with file operations.
+ *
+ * @since 1.4.0
+ * @author HMS Networks, MU Americas Solution Center
+ */
+public class FileConstants {
+
+  /** URL prefix for local files */
+  public static final String FILE_URL_PREFIX = "file://";
+}
