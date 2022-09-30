390ab2fa291c502093c6d0bf4b55e058b3384d05
Alex J Hawk (HMS) <alha@hms.se>
2022-09-30 15:19:23 -0400

    Add app args parser class
    This class allows for easy parsing
    and interpretation of the values
    indicated by possible application
    arguments.
    
    This class has been developed in a
    manner such that it can easily be
    expanded or built upon in the future.

---
+++ src/main/java/com/hms_networks/americas/sc/extensions/system/application/SCAppArgsParser.java
@@ -0,0 +1,62 @@
+package com.hms_networks.americas.sc.extensions.system.application;
+
+/**
+ * A utility class for parsing the arguments passed to the main method of an Ewon Flexy Java
+ * application, and providing methods for checking the values of common/possible arguments (or the
+ * default value, if applicable).
+ *
+ * @author HMS Networks, MU Americas Solution Center
+ * @since 1.10.0
+ * @version 1.0.0
+ */
+public class SCAppArgsParser {
+
+  /**
+   * The argument specified to an Ewon Flexy Java application to indicate that it was started by the
+   * Ewon Flexy Java Multi-Loader application.
+   *
+   * @since 1.0.0
+   */
+  public static final String ARG_STARTED_BY_MULTI_LOADER = "-StartedByMultiLoader";
+
+  /**
+   * The boolean value used to indicate if the application was started by the Ewon Flexy Java
+   * Multi-Loader application. This value is set to true if the {@link #ARG_STARTED_BY_MULTI_LOADER}
+   * argument is included in the application arguments.
+   *
+   * @since 1.0.0
+   */
+  private boolean startedByMultiLoader = false;
+
+  /**
+   * Parses the specified arguments (passed to the main method of an Ewon Flexy Java application)
+   * and sets the values of the appropriate field.
+   *
+   * @param args the arguments passed to the main method of an Ewon Flexy Java application
+   * @since 1.0.0
+   */
+  public SCAppArgsParser(String[] args) {
+    // Loop through to check for known arguments and store their values
+    for (int i = 0; i < args.length; i++) {
+
+      // Set the startedByMultiLoader value if the argument is found
+      if (args[i].equals(ARG_STARTED_BY_MULTI_LOADER)) {
+        startedByMultiLoader = true;
+      }
+    }
+  }
+
+  /**
+   * Gets a boolean value indicating if the application was started by the Ewon Flexy Java
+   * Multi-Loader application.
+   *
+   * @return the boolean value indicating if the application was started by the Ewon Flexy Java
+   *     Multi-Loader application. This value is set to true if the {@link
+   *     #ARG_STARTED_BY_MULTI_LOADER} argument is included in the application arguments. If the
+   *     argument is not included, this value is set to false.
+   * @since 1.0.0
+   */
+  public boolean getStartedByMultiLoader() {
+    return startedByMultiLoader;
+  }
+}
