4328ad1f81536d387d8f0be0c1b6cc132c7fdb3d
Tom Kimsey <thk@hms.se>
2023-06-06 15:59:10 -0400

    Created SCAppConstants.java
    Moved reusable constants to constants file.

---
+++ src/main/java/com/hms_networks/americas/sc/extensions/system/application/SCAppConstants.java
@@ -0,0 +1,14 @@
+package com.hms_networks.americas.sc.extensions.system.application;
+
+/**
+ * Class for storing constants used in the {@link
+ * com.hms_networks.americas.sc.extensions.system.application} package.
+ *
+ * @since 1.14.0
+ * @author HMS Networks, MU Americas Solution Center
+ */
+public class SCAppConstants {
+
+  /** File path for jvmrun file */
+  public static final String JVM_RUN_PATH = "/usr/jvmrun";
+}
