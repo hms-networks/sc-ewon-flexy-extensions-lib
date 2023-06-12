df539669a7d9c796d5535a5b0f684bb053517e21
Alex J Hawk (HMS) <51409174+alexjhawk@users.noreply.github.com>
2023-06-12 11:36:28 -0400

    Rescope HistoricalDataConstants.java fields
    Make the fields within the HistoricalDataConstants.java
    class public. There is no technical requirement for these
    to be private, package-private, or protected.
    
    Future additions to this class require public access, so
    it makes sense to prepare the class so all fields are
    consistent.

--- src/main/java/com/hms_networks/americas/sc/extensions/historicaldata/HistoricalDataConstants.java
+++ src/main/java/com/hms_networks/americas/sc/extensions/historicaldata/HistoricalDataConstants.java
@@ -9,59 +9,59 @@ package com.hms_networks.americas.sc.extensions.historicaldata;
 class HistoricalDataConstants {
 
   /** Delimiter used in EBD lines */
-  static final String EBD_LINE_DELIMITER = ";";
+  public static final String EBD_LINE_DELIMITER = ";";
 
   /** Index of tag ID in EBD lines */
-  static final int EBD_LINE_TAG_ID_INDEX = 0;
+  public static final int EBD_LINE_TAG_ID_INDEX = 0;
 
   /** Index of time int in EBD lines */
-  static final int EBD_LINE_TAG_TIMEINT_INDEX = 1;
+  public static final int EBD_LINE_TAG_TIMEINT_INDEX = 1;
 
   /** Index of tag value in EBD lines */
-  static final int EBD_LINE_TAG_VALUE_INDEX = 4;
+  public static final int EBD_LINE_TAG_VALUE_INDEX = 4;
 
   /** Length of EBD lines */
-  static final int EBD_LINE_LENGTH = 6;
+  public static final int EBD_LINE_LENGTH = 6;
 
   /** Time format used for EBD files */
-  static final String EBD_TIME_FORMAT = "ddMMyyyy_HHmmss";
+  public static final String EBD_TIME_FORMAT = "ddMMyyyy_HHmmss";
 
   /** Folder to store queue files */
-  static final String QUEUE_FILE_FOLDER = "/usr/hist-data-queue";
+  public static final String QUEUE_FILE_FOLDER = "/usr/hist-data-queue";
 
   /**
    * Extension to use for queue files. Note, storing with a non-standard extension reduces the risk
    * of manual file tampering.
    */
-  static final String QUEUE_FILE_EXTENSION = ".q";
+  public static final String QUEUE_FILE_EXTENSION = ".q";
 
   /** Name of EBD export call file */
-  static final String QUEUE_EBD_FILE_NAME = "histDataEBD";
+  public static final String QUEUE_EBD_FILE_NAME = "histDataEBD";
 
   /** Name of EBD string export call file */
-  static final String QUEUE_EBD_STRING_FILE_NAME = "histStringDataEBD";
+  public static final String QUEUE_EBD_STRING_FILE_NAME = "histStringDataEBD";
 
   /** Name of historical data queue time tracker file 1. */
-  static final String QUEUE_TIME_FILE_1_NAME = "histDataTime1";
+  public static final String QUEUE_TIME_FILE_1_NAME = "histDataTime1";
 
   /** Name of historical data queue time tracker file 2. */
-  static final String QUEUE_TIME_FILE_2_NAME = "histDataTime2";
+  public static final String QUEUE_TIME_FILE_2_NAME = "histDataTime2";
 
   /** Minimum amount of Historical FIFO queue that can be set by library users. */
-  static final long MIN_QUEUE_SPAN_MINS = 1;
+  public static final long MIN_QUEUE_SPAN_MINS = 1;
 
   /** Default thread sleep time waiting for EBD in ms. */
-  static final int DEFAULT_EBD_THREAD_SLEEP_MS = 5;
+  public static final int DEFAULT_EBD_THREAD_SLEEP_MS = 5;
 
   /** Maximum amount of time that EBD call should wait in milliseconds before timing out. */
-  static final long MAX_EBD_WAIT_MS = 15000;
+  public static final long MAX_EBD_WAIT_MS = 15000;
 
   /** Tag value representing the negative infinity Float/Double value. */
-  static final String TAG_VALUE_NEGATIVE_INFINITY = "-inf";
+  public static final String TAG_VALUE_NEGATIVE_INFINITY = "-inf";
 
   /** Tag value representing the positive infinity Float/Double value. */
-  static final String TAG_VALUE_POSITIVE_INFINITY = "inf";
+  public static final String TAG_VALUE_POSITIVE_INFINITY = "inf";
 
   /** Tag value representing the NaN Float/Double value. */
-  static final String TAG_VALUE_NAN = "nan";
+  public static final String TAG_VALUE_NAN = "nan";
 }
