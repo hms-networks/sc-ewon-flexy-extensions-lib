7193d4bb31a5365efe48db639d67b6ce3a0cadb4
Alex J Hawk (HMS) <51409174+alexjhawk@users.noreply.github.com>
2023-09-05 12:10:44 -0400

    Add combined logging methods
    Added combined logging methods in Logger.java which
    allow for a log string to be passed alongside an
    exception. There is the ability to both output an
    exception at the same log level as the associated
    string, or at the exception/trace log level.

--- src/main/java/com/hms_networks/americas/sc/extensions/logging/Logger.java
+++ src/main/java/com/hms_networks/americas/sc/extensions/logging/Logger.java
@@ -312,12 +312,59 @@ public class Logger {
   }
 
   /**
+   * Log a string and exception with the debug log level
+   *
+   * @param logString string to log
+   * @param logException exception to log
+   * @since 1.15.0
+   */
+  public static void LOG_DEBUG(String logString, Exception logException) {
+    LOG(LOG_LEVEL_DEBUG, logString);
+    LOG_EXCEPTION(LOG_LEVEL_DEBUG, logException);
+  }
+
+  /**
+   * Log a string with the debug log level and exception with the trace log level
+   *
+   * @param logString string to log
+   * @param logException exception to log
+   * @since 1.15.0
+   */
+  public static void LOG_DEBUG_EXCEPTION(String logString, Exception logException) {
+    LOG(LOG_LEVEL_DEBUG, logString);
+    LOG_EXCEPTION(logException);
+  }
+
+  /**
    * Log a string with the info log level
    *
    * @param logString string to log
    */
   public static void LOG_INFO(String logString) {
     LOG(LOG_LEVEL_INFO, logString);
+  }
+
+  /**
+   * Log a string and exception with the info log level
+   *
+   * @param logString string to log
+   * @since 1.15.0
+   */
+  public static void LOG_INFO(String logString, Exception logException) {
+    LOG(LOG_LEVEL_INFO, logString);
+    LOG_EXCEPTION(LOG_LEVEL_INFO, logException);
+  }
+
+  /**
+   * Log a string with the info log level and exception with the trace log level
+   *
+   * @param logString string to log
+   * @param logException exception to log
+   * @since 1.15.0
+   */
+  public static void LOG_INFO_EXCEPTION(String logString, Exception logException) {
+    LOG(LOG_LEVEL_INFO, logString);
+    LOG_EXCEPTION(logException);
   }
 
   /**
@@ -330,6 +377,29 @@ public class Logger {
   }
 
   /**
+   * Log a string and exception with the warning log level
+   *
+   * @param logString string to log
+   * @since 1.15.0
+   */
+  public static void LOG_WARN(String logString, Exception logException) {
+    LOG(LOG_LEVEL_WARN, logString);
+    LOG_EXCEPTION(LOG_LEVEL_WARN, logException);
+  }
+
+  /**
+   * Log a string with the warning log level and exception with the trace log level
+   *
+   * @param logString string to log
+   * @param logException exception to log
+   * @since 1.15.0
+   */
+  public static void LOG_WARN_EXCEPTION(String logString, Exception logException) {
+    LOG(LOG_LEVEL_WARN, logString);
+    LOG_EXCEPTION(logException);
+  }
+
+  /**
    * Log a string with the serious log level
    *
    * @param logString string to log
@@ -339,12 +409,58 @@ public class Logger {
   }
 
   /**
+   * Log a string and exception with the serious log level
+   *
+   * @param logString string to log
+   * @since 1.15.0
+   */
+  public static void LOG_SERIOUS(String logString, Exception logException) {
+    LOG(LOG_LEVEL_SERIOUS, logString);
+    LOG_EXCEPTION(LOG_LEVEL_SERIOUS, logException);
+  }
+
+  /**
+   * Log a string with the serious log level and exception with the trace log level
+   *
+   * @param logString string to log
+   * @param logException exception to log
+   * @since 1.15.0
+   */
+  public static void LOG_SERIOUS_EXCEPTION(String logString, Exception logException) {
+    LOG(LOG_LEVEL_SERIOUS, logString);
+    LOG_EXCEPTION(logException);
+  }
+
+  /**
    * Log a string with the critical log level
    *
    * @param logString string to log
    */
   public static void LOG_CRITICAL(String logString) {
     LOG(LOG_LEVEL_CRITICAL, logString);
+  }
+
+  /**
+   * Log a string and exception with the critical log level
+   *
+   * @param logString string to log
+   * @since 1.15.0
+   */
+  public static void LOG_CRITICAL(String logString, Exception logException) {
+    LOG(LOG_LEVEL_CRITICAL, logString);
+    LOG_EXCEPTION(LOG_LEVEL_CRITICAL, logException);
+  }
+
+  /**
+   * Log a string with the critical log level and exception with the trace log level
+   *
+   * @param logString string to log
+   * @param logException exception to log
+   * @since 1.15.0
+   */
+  public static void LOG_CRITICAL_EXCEPTION(String logString, Exception logException) {
+    LOG(LOG_LEVEL_CRITICAL, logString);
+    LOG_EXCEPTION(logException);
   }
 
   /** Output all queued logs at critical level */
