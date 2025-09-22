cd351e0f984c3f88b8a1315cb42612b3e37dc487
Ian Tracy <iatr@hms.se>
2025-09-22 13:40:11 -0400

    RELEASE: v1.16.4

--- src/main/java/com/hms_networks/americas/sc/extensions/ExtensionsMain.java
+++ src/main/java/com/hms_networks/americas/sc/extensions/ExtensionsMain.java
@@ -1,5 +1,9 @@
 package com.hms_networks.americas.sc.extensions;
 
+import com.hms_networks.americas.sc.extensions.datapoint.DataPoint;
+import com.hms_networks.americas.sc.extensions.historicaldata.HistoricalDataQueueManager;
+import java.util.ArrayList;
+
 /**
  * Main class for the project. This class does not perform any functions, as it is contained in a
  * library.
@@ -9,23 +13,39 @@ package com.hms_networks.americas.sc.extensions;
  */
 public class ExtensionsMain {
 
-  /**
+  public static void runTest() {
+    int i = 0;
+    boolean startNewTimeTracker = true;
+    ArrayList datapointsReadFromQueue = null;
+    try {
+      for (i = 0; i < 20; i++) {
+
+        datapointsReadFromQueue =
+            HistoricalDataQueueManager.getFifoNextSpanDataAllGroups(startNewTimeTracker);
+        i++;
+        startNewTimeTracker = false;
+        for (int j = 0; j < datapointsReadFromQueue.size(); j++) {
+          DataPoint dp = (DataPoint) datapointsReadFromQueue.get(j);
+          System.out.println(
+              "Datapoint "
+                  + dp.getTagName()
+                  + " timestamp: "
+                  + dp.getIso8601Timestamp()
+                  + " IsInit value: "
+                  + dp.isInitValue());
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /*
    * Main method for the project main class.
    *
    * @param args program arguments
    */
   public static void main(String[] args) {
-    // Get project information
-    String projectName = ExtensionsMain.class.getPackage().getImplementationTitle();
-    String projectVersion = ExtensionsMain.class.getPackage().getImplementationVersion();
-
-    // Display error message -- not a runnable Jar
-    System.err.println(
-        "The project "
-            + projectName
-            + " v"
-            + projectVersion
-            + " is not a runnable Jar application and should be referenced using the Java"
-            + " classpath.");
+    System.out.println("Running test version that will demonstrate the new IsInit value.");
   }
 }
